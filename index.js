8c2624b6242aff3b94903c67c0caba067c0cb094
ShoroveTajmen <sorovitajmen@gmail.com>
2023-11-27 05:41:51 +0600

    delete method added for delete specific product

--- index.js
+++ index.js
@@ -132,6 +132,16 @@ async function run() {
       res.send(result);
     });
 
+
+    //delete specific product
+    app.delete('/product/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await productsCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     // console.log(
